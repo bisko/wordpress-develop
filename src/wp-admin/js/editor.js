23b227290fd6c471dbfea587be12815a22935278
Biser Perchinkov <biser@bisko.be>
2017-09-22 00:06:53 +0300

    restore whitespace formatting

--- src/wp-admin/js/editor.js
+++ src/wp-admin/js/editor.js
@@ -948,16 +948,16 @@ window.wp = window.wp || {};
 			var $textarea = $( '#' + id );
 
 			var $wrap = $( '<div>' ).attr( {
-						'class': 'wp-core-ui wp-editor-wrap tmce-active',
-						id: 'wp-' + id + '-wrap'
-					} );
+					'class': 'wp-core-ui wp-editor-wrap tmce-active',
+					id: 'wp-' + id + '-wrap'
+				} );
 
 			var $editorContainer = $( '<div class="wp-editor-container">' );
 
 			var $button = $( '<button>' ).attr( {
-							type: 'button',
-							'data-wp-editor-id': id
-						} );
+					type: 'button',
+					'data-wp-editor-id': id
+				} );
 
 			var $editorTools = $( '<div class="wp-editor-tools">' );
 
